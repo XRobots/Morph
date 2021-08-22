37b8b6eff7a942347ac9efcbe4138ffe47ae4c6d
James Bruton <xrobots@gmail.com>
2021-08-22 19:22:19 +0100

    Add files via upload

---
+++ Code/001/001.ino
@@ -0,0 +1,75 @@
+#include <Servo.h>
+
+int count1;
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+Servo servo5;
+Servo servo6;
+Servo servo7;
+Servo servo8;
+
+void setup() {
+
+  servo1.attach(2);
+  servo2.attach(3);
+  servo3.attach(4);
+  servo4.attach(5);
+  servo5.attach(6);
+  servo6.attach(7);
+  servo7.attach(8);
+  servo8.attach(9);
+
+  servo1.write(90); 
+  servo2.write(90);
+  servo3.write(90);
+  servo4.write(90);
+  servo5.write(90);
+  servo6.write(90);
+  servo7.write(90);
+  servo8.write(90);
+
+  delay(2000);
+
+}
+
+void loop() {
+
+  while(count1 <= 7) {
+  
+    servo1.write(50);     // outwards
+    servo7.write(50);
+
+    servo3.write(130);    // inwards
+    servo5.write(130);
+  
+    delay(600);
+
+    servo1.write(130);     // outwards
+    servo7.write(130);
+
+    servo3.write(50);    // inwards
+    servo5.write(50);
+
+    delay (600);
+  
+    count1 = count1 + 1;
+
+  }
+  
+    servo1.write(90); 
+    servo2.write(90);
+    servo3.write(90);
+    servo4.write(90);
+    servo5.write(90);
+    servo6.write(90);
+    servo7.write(90);
+    servo8.write(90);
+
+
+
+
+
+}
